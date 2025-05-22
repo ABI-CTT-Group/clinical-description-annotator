cdba70ebef5d8cf9a146b5e9f7615117dcbb2a48
LinkunGao <gaolinkun123@gmail.com>
2025-05-22 18:30:08 +0800

    Make the description variable private.

--- fhir_cda/annotator/abstract_annotator.py
+++ fhir_cda/annotator/abstract_annotator.py
@@ -6,15 +6,15 @@ import json
 class AbstractAnnotator(ABC):
 
     def __init__(self, dataset_path, category):
-        self.root = Path(dataset_path)
-        self.category = category
-        self.descriptions = {}
+        self._root = Path(dataset_path)
+        self._category = category
+        self._descriptions = {}
 
     def save(self, path=None):
         if path:
-            save_path = Path(path) / f"{self.category}.json"
+            save_path = Path(path) / f"{self._category}.json"
         else:
-            save_path = self.root / f"{self.category}.json"
+            save_path = self._root / f"{self._category}.json"
 
         with open(save_path, "w") as json_file:
-            json.dump(self.descriptions, json_file, indent=4)
+            json.dump(self._descriptions, json_file, indent=4)
