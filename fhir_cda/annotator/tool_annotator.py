0d69c89a79166f17537d8d51195a0fcaf5b6f74f
LinkunGao <gaolinkun123@gmail.com>
2025-09-21 01:15:21 +1200

    update to uv and start to develop the workflow tool annotator

--- fhir_cda/annotator/tool_annotator.py
+++ fhir_cda/annotator/tool_annotator.py
@@ -1,4 +1,5 @@
-
-class ToolAnnotator:
-    def __init__(self):
-        pass
+from abc import ABC
+from .abstract_annotator import AbstractAnnotator
+class ToolAnnotator(AbstractAnnotator, ABC):
+    def __init__(self, dataset_path):
+        super().__init__(dataset_path, "workflow_tool")
