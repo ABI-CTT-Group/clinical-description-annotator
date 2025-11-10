8cb5319862f8ba5a7c3a565b2314eacd0bb29a03
LinkunGao <gaolinkun123@gmail.com>
2025-11-10 23:10:42 +1300

    v1.2.3

--- fhir_cda/__init__.py
+++ fhir_cda/__init__.py
@@ -2,8 +2,8 @@ from .core import Annotator
 from datetime import datetime
 
 __title__ = "fhir_cda"
-__version__ = "1.2.2"
-__author__ = "Linun Gao"
+__version__ = "1.2.3"
+__author__ = "Linkun Gao"
 __license__ = "Apache-2.0"
 __copyright__ = f"Copyright 2024-{datetime.now().year} ABI"
 
